2466f59c7296459d23f20e74c4214077997be655
Javier Ledesma <javier@ledesma.ch>
2022-07-02 15:50:45 +0200

    Only one instance type

--- Device/module.php
+++ Device/module.php
@@ -1,17 +1,17 @@
 <?php
 
 declare(strict_types=1);
-require_once __DIR__ . '/../libs/ColorHelper.php';
+// require_once __DIR__ . '/../libs/ColorHelper.php';
 require_once __DIR__ . '/../libs/MQTTHelper.php';
-require_once __DIR__ . '/../libs/VariableProfileHelper.php';
-require_once __DIR__ . '/../libs/Zigbee2MQTTHelper.php';
+// require_once __DIR__ . '/../libs/VariableProfileHelper.php';
+require_once __DIR__ . '/../libs/CommandHelper.php';
 
 class Zwavejs2MqttDevice extends IPSModule
 {
-    use ColorHelper;
+    // use ColorHelper;
     use MQTTHelper;
-    use VariableProfileHelper;
-    use Zigbee2MQTTHelper;
+    // use VariableProfileHelper;
+    use CommandHelper;
 
     public function Create()
     {
@@ -20,7 +20,8 @@ class Zwavejs2MqttDevice extends IPSModule
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
 
         $this->RegisterPropertyString('MQTTTopic', '');
-        $this->createVariableProfiles();
+        $this->RegisterPropertyString("Associations", '[]');
+        // $this->createVariableProfiles();
     }
 
     public function ApplyChanges()
@@ -29,7 +30,11 @@ class Zwavejs2MqttDevice extends IPSModule
         parent::ApplyChanges();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
         //Setze Filter für ReceiveData
-        $MQTTTopic = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic');
-        $this->SetReceiveDataFilter('.*' . $MQTTTopic . '".*');
+        $topic = $this->ReadPropertyString('MQTTTopic');
+        $filter = '.*"zwave/' . $topic . '.*';
+        $this->SetReceiveDataFilter($filter);
+
+        $this->initVariables();
     }
+
 }
--- libs/CommandHelper.php
+++ libs/CommandHelper.php
@@ -5,30 +5,69 @@ declare(strict_types=1);
 trait CommandHelper
 {
 
-    public function SwitchMode($state) {
-        if(topicContains("switch_multilevel")) {
+    public function RequestAction($ident, $value)
+    {
+        switch($ident) {
+            case 'ZW2M_State': 
+                $this->SwitchMode($value);
+                break;
+            case 'ZW2M_Brightness':
+                $this->DimSet($value);
+                break;
+        }          
+    }    
+
+    public function ReceiveData($json)
+    {
+        $this->SendDebug('JSON', $json, 0);
+        if (!empty($this->ReadPropertyString('MQTTTopic'))) {
+            $buffer = json_decode($json);
+            // buffer decodieren und in eine Variable schreiben
+            $this->SendDebug('MQTT Topic', $buffer->Topic, 0);
+            $this->SendDebug('MQTT Payload', $buffer->Payload, 0);
+            if(fnmatch('*/currentValue', $buffer->Topic)){
+                $value = $buffer->Payload;
+                if($this->topicContains("switch_multilevel")) {
+                    SetValue($this->GetIDForIdent('ZW2M_State'), $value > 0);
+                    SetValue($this->GetIDForIdent('ZW2M_Brightness'), $value);
+                }
+                else if($this->topicContains("switch_binary")) {
+                    SetValue($this->GetIDForIdent('ZW2M_State'), $value);
+                }
+            }
+        }
+    }
+
+    public function SwitchMode(bool $state) {
+        if($this->topicContains("switch_multilevel")) {
             $this->sendEvent($state ? 100 : 0);
         } 
-        else if(topicContains("switch_binary")) {
+        else if($this->topicContains("switch_binary")) {
             $this->sendEvent($state ? "on" : "off");
         }
         else {
             $this->LogMessage("Instanz kann nicht switchen", KL_WARNING);
         }
+        $this->notifyAssociations(function($id, $val) {
+            ZW2M_SwitchMode($id, $val);
+        }, $state);
     }
 
-    public function DimSet($value) {
-        if(topicContains("switch_multilevel")) {
+    public function DimSet(int $value) {
+        if($this->topicContains("switch_multilevel")) {
             $this->sendEvent($value);
         } 
         else {
             $this->LogMessage("Instanz kann nicht dimmen", KL_WARNING);
         }
+        $this->notifyAssociations(function($id, $val) {
+            ZW2M_DimSet($id, $val);
+        }, $value);
     }
 
     private function topicContains($keyword) {
         $topic = $this->ReadPropertyString('MQTTTopic');
-        return preg_match('\b'.$keyword'\b');
+        return preg_match("/\b".$keyword."\b/", $topic);
     }
 
     private function sendEvent($newValue)
@@ -43,8 +82,32 @@ trait CommandHelper
         $this->SendDebug(__FUNCTION__ . ' Topic', $Data['Topic'], 0);
         $this->SendDebug(__FUNCTION__ . ' Payload', $Data['Payload'], 0);
         $this->SendDataToParent($DataJSON);
-
-        $this->notifyAssociations($newValue);
     }
+
+
+    private function notifyAssociations(callable $callback, $newValue) {
+        $associations = json_decode($this->ReadPropertyString('Associations'));
+        if (is_array($associations) || is_object($associations)) {
+            foreach($associations as $association) {
+                $callback($association->InstanceID, $newValue);
+            }
+        }
+    }
+
+    private function initVariables() {
+        if($this->topicContains("switch_multilevel")) {
+            // DIMMER
+            $this->RegisterVariableBoolean('ZW2M_State', $this->Translate('State'), '~Switch', 0);
+            $this->EnableAction("ZW2M_State");
+            $this->RegisterVariableInteger('ZW2M_Brightness', $this->Translate('Brightness'), '~Intensity.100', 1);
+            $this->EnableAction("ZW2M_Brightness");
+        } 
+        else if($this->topicContains("switch_binary")) {
+            // SWITCH
+            $this->RegisterVariableBoolean('ZW2M_State', $this->Translate('State'), '~Switch', 0);
+            $this->EnableAction("ZW2M_State");
+        }
+    }
+
 
 }
